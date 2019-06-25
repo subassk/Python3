5c8dd0255062d901b530c7171389ce763a741f44
DA2017 <31857664+subassk@users.noreply.github.com>
2019-06-24 23:48:07 -0400

    Update Class_Object_Inheritance.py

--- Class_Object_Inheritance.py
+++ Class_Object_Inheritance.py
@@ -56,13 +56,10 @@ class BankAccount:
 		self.name = name
 		self.amt = amt
 	def __str__(self):
-		return "Your account, {}, has {:} dollars.".format(self.name, self.amt)
+		return "Your account, {}, has {} dollars.".format(self.name, self.amt)
+        #float amount
+        #return "Your account, {}, has {1:.1f} dollars.".format(self.name, self.amt)
 
-
-
-
-
-
-
-
+t1 = BankAccount("Bob", 100.0)
+print(t1)
 
