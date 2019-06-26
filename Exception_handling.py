599edb1433daaf073ad132cfb3b21c84a1680b39
DA2017 <31857664+subassk@users.noreply.github.com>
2019-06-26 00:07:50 -0400

    Update Exception_handling.py

--- Exception_handling.py
+++ Exception_handling.py
@@ -114,9 +114,24 @@ You passed: 100.0% of the tests
     
 //The following code tries to append the third element of each list in conts to the new list third_countries. Currently, the code does not work. Add a try/except clause so the code runs without errors, and the string ‘Continent does not have 3 countries’ is appended to countries instead of producing an error.    
     
+conts = [['Spain', 'France', 'Greece', 'Portugal', 'Romania', 'Germany'], ['USA', 'Mexico', 'Canada'], ['Japan', 'China', 'Korea', 'Vietnam', 'Cambodia'], ['Argentina', 'Chile', 'Brazil', 'Ecuador', 'Uruguay', 'Venezuela'], ['Australia'], ['Zimbabwe', 'Morocco', 'Kenya', 'Ethiopa', 'South Africa'], ['Antarctica']]
+
+third_countries = []
+
+for c in conts:
+    try:
+        third_countries.append(c[2])
+    except:
+        third_countries.append("Continent does not have 3 countries")
+        
+print(third_countries)
     
     
-    
+['Greece', 'Canada', 'Korea', 'Brazil', 'Continent does not have 3 countries', 'Kenya', 'Continent does not have 3 countries']
+
+Result	Actual Value	Expected Value	Notes
+Pass	"['Gre...ies']"	"['Gre...ies']"	Testing that third_countries is created correctly.
+You passed: 100.0% of the tests    
     
     
     
