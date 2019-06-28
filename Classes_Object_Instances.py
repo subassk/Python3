87d49da2538fe2aedbe49821d201ac56c408a3ac
DA2017 <31857664+subassk@users.noreply.github.com>
2019-06-27 23:58:10 -0400

    Update Classes_Object_Instances.py

--- Classes_Object_Instances.py
+++ Classes_Object_Instances.py
@@ -65,3 +65,6 @@ class BankAccount:
 t1 = BankAccount("Bob", 100.0)
 print(t1)
 
+
+
+//Create a class called Animal that accepts two numbers as inputs and assigns them respectively to two instance variables: arms and legs. Create an instance method called limbs that, when called, returns the total number of limbs the animal has. To the variable name spider, assign an instance of Animal that has 4 arms and 4 legs. Call the limbs method on the spider instance and save the result to the variable name spidlimbs.
